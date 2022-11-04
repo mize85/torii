4f40f519ab6d04c9c5169291249753262eac462f
Jan Krutisch <jan@krutisch.de>
2022-11-04 19:14:19 +0100

    Remove last autoinjection (wasn't needed anyway)

--- addon/bootstrap/session.js
+++ addon/bootstrap/session.js
@@ -3,5 +3,4 @@ import ToriiSessionService from 'torii/services/torii-session';
 export default function (application, sessionName) {
   var sessionFactoryName = 'service:' + sessionName;
   application.register(sessionFactoryName, ToriiSessionService);
-  application.inject(sessionFactoryName, 'torii', 'service:torii');
 }
